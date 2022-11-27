6d7b57d48c8c52ca85c41453e4f839d30ace32b2
Rishu Tripathi <rtrp.edu@gmail.com>
2022-11-27 20:16:25 +0530

    CORS policy added

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const cors=require('cors');
 require('dotenv').config();
 require('./model/model.js');
 const path=require('path');
@@ -7,8 +8,10 @@ app.use(express.json());
 const route=require('./routes/route.js');
 const show=require('./routes/show.js');
 const download=require('./routes/download.js');
-
-app.use(route,show,download);
+const corsOption={
+    origin: process.env.CLIENTS.split(","),
+}
+app.use(route,show,download,cors(corsOption));
 app.set('view engine','ejs');
 app.set('views',path.resolve(__dirname,'views'));
 app.use(express.static(path.join(__dirname)));
